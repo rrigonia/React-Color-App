b1fe70194ca8dd3bb83f4ba79de0d48c4c852e24
Ramon Rigoni <rrigoni.a@gmail.com>
2021-08-17 17:04:59 -0300

    Adjust Text Color Based On Luminance

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./ColorBox.css";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { withRouter } from "react-router";
+import chroma from "chroma-js";
 
 class ColorBox extends Component {
 	constructor(props) {
@@ -15,13 +16,16 @@ class ColorBox extends Component {
 			setTimeout(() => this.setState({ copied: false }), 1500);
 		});
 	}
-	goToMore(e){
+	goToMore(e) {
 		e.stopPropagation();
-		this.props.history.push(`/palettes/${this.props.palette}/${this.props.id}`)
+		this.props.history.push(`/palettes/${this.props.palette}/${this.props.id}`);
 	}
 	render() {
-		const { name, background,showLink } = this.props;
+		const { name, background, showLink } = this.props;
 		const { copied } = this.state;
+		const isDarkColor = chroma(background).luminance() <= 0.1;
+		const isLightColor = chroma(background).luminance() >= 0.6;
+
 		return (
 			<CopyToClipboard text={background} onCopy={this.changeCopyState}>
 				<div className="ColorBox" style={{ background }}>
@@ -30,19 +34,27 @@ class ColorBox extends Component {
 						className={`copy-overlay ${copied && "show"}`}
 					/>
 					<div className={`copy-msg ${copied && "show"}`}>
-						<h1>Copied!</h1>
-						<p>{background}</p>
+						<h1 className={isLightColor ? "dark-text" : undefined}>Copied!</h1>
+						<p className={isLightColor ? "dark-text" : undefined}>
+							{background}
+						</p>
 					</div>
 					<div className="copy-container">
 						<div className="box-content">
-							<span>{name}</span>
+							<span className={isDarkColor ? "light-text" : undefined}>
+								{name}
+							</span>
 						</div>
-						<button className="copy-button">Copy</button>
+						<button className={`copy-button ${isLightColor ? "dark-text" : undefined}`}>Copy</button>
 					</div>
 					{showLink && (
-						<span onClick={this.goToMore} className="see-more">More</span>
+						<span
+							onClick={this.goToMore}
+							className={`see-more ${isLightColor ? "dark-text" : undefined}`}
+						>
+							More
+						</span>
 					)}
-					
 				</div>
 			</CopyToClipboard>
 		);
