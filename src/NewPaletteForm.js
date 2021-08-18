604311e929389628b97ea9b2b2df3540ac1fdaaf
Ramon Rigoni <rrigoni.a@gmail.com>
2021-08-18 17:48:54 -0300

    Add ChromePicker and Buttons to Drawer

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -10,9 +10,11 @@ import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import { ChromePicker } from 'react-color';
+import { Button } from '@material-ui/core';
 
 
-const drawerWidth = 240;
+const drawerWidth = 400;
 
 const styles = theme => ({
   root: {
@@ -74,6 +76,7 @@ const styles = theme => ({
 class NewPaletteForm extends Component {
     state = {
         open: false,
+        background: "#fff"
       };
     
       handleDrawerOpen = () => {
@@ -82,6 +85,10 @@ class NewPaletteForm extends Component {
     
       handleDrawerClose = () => {
         this.setState({ open: false });
+      };
+
+      handleChangeComplete = (color) => {
+        this.setState({ background: color.hex });
       };
     
       render() {
@@ -126,6 +133,19 @@ class NewPaletteForm extends Component {
                 </IconButton>
               </div>
               <Divider />
+              <Typography variant="h4">
+                  Design Your Palette
+              </Typography>
+              <div>
+                  <Button variant="contained" color="secondary">
+                      Clear Palette
+                  </Button>
+                  <Button variant="contained" color="primary">
+                      Random Color
+                  </Button>
+              </div>
+              <ChromePicker color={this.state.background} onChangeComplete={this.handleChangeComplete}  />
+              <Button variant="contained" color="primary">Add Color</Button>
             </Drawer>
             <main
               className={classNames(classes.content, {
@@ -133,7 +153,6 @@ class NewPaletteForm extends Component {
               })}
             >
               <div className={classes.drawerHeader} />
-              
             </main>
           </div>
         );
