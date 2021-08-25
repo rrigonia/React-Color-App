3278ba3a232127f9cf4aead40f622d66dbe5e705
Ramon Rigoni <rrigoni.a@gmail.com>
2021-08-24 23:12:41 -0300

    Add bg svg to PaletteList

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,56 +1,62 @@
-import sizes from "./sizes"
+import sizes from "./sizes";
+import bg from "./bg.svg";
 
 const styles = {
-    root: {
-        backgroundColor: "blue",
-        minHeight: "100vh",
-        display: "flex",
-        alignItems: "flex-start",
-        justifyContent: "center"
-    },
-    container: {
-        width: "50%",
-        display: "flex",
-        flexDirection: "column",
-        flexWrap: "wrap",
-        alignItems: "flex-start",
-        [sizes.down("xl")]:{
-            width: "80%"
-        },
-        [sizes.down("xs")]:{
-            width: "70%"
-        }
-
-    },
-    nav: {
-        display: "flex",
-        width: "100%",
-        justifyContent: "space-between",
-        alignItems: "center",
-        color: "white",
-        "& h1":{
-            fontSize: "1.5rem",
-            padding: "1rem 0"
-        },
-        "& a":{
-            color: "white"
-        }
-        // color: "white"
-    },
-    palettes: {
-        boxSizing: "border-box",
-        width: "100%",
-        display: "grid",
-        gridTemplateColumns: "repeat(3, 30%)",
-        gridGap: "2.5rem",
-        [sizes.down("md")]: {
-            gridTemplateColumns: "repeat(2, 50%)"
-        },
-        [sizes.down("xs")]: {
-            gridTemplateColumns: "repeat(1, 100%)",
-            gridGap: "1rem"
-        }
-    }
-}
+	root: {
+		backgroundColor: "blue",
+		height: "100vh",
+		display: "flex",
+		alignItems: "flex-start",
+		justifyContent: "center",
+		/* background by SVGBackgrounds.com */
+		backgroundColor: "#5D4EFF",
+		backgroundImage: `url(${bg})`,
+		overflow: "auto"
+	},
+	container: {
+		width: "50%",
+		display: "flex",
+		flexDirection: "column",
+		flexWrap: "wrap",
+		alignItems: "flex-start",
+		marginBottom: "1rem",
+		[sizes.down("xl")]: {
+			width: "80%"
+		},
+		[sizes.down("xs")]: {
+			width: "70%"
+		}
+	},
+	nav: {
+		display: "flex",
+		width: "100%",
+		justifyContent: "space-between",
+		alignItems: "center",
+		color: "white",
+		"& h1": {
+			fontSize: "2rem",
+            fontWeight: "bold",
+			padding: "1rem 0"
+		},
+		"& a": {
+			color: "white"
+		}
+		// color: "white"
+	},
+	palettes: {
+		boxSizing: "border-box",
+		width: "100%",
+		display: "grid",
+		gridTemplateColumns: "repeat(3, 30%)",
+		gridGap: "2.5rem",
+		[sizes.down("md")]: {
+			gridTemplateColumns: "repeat(2, 50%)"
+		},
+		[sizes.down("xs")]: {
+			gridTemplateColumns: "repeat(1, 100%)",
+			gridGap: "1.4rem"
+		}
+	}
+};
 
 export default styles;
